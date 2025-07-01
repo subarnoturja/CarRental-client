b9bb6021ba8315b6d5e9a342141e74313cf62e66
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-01 21:42:13 +0600

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
 import Navbar from "./components/Navbar"
-import { useLocation } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
+import Home from "./pages/Home";
+import CarDetails from "./pages/CarDetails";
+import { Cars } from "./pages/Cars";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
 
@@ -10,6 +14,12 @@ function App() {
   return (
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
     </>
   )
 }
