c3234e6d79a1228df5453e0e90d0148e0e5d054d
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-10 12:29:52 +0600

    dashboard added

--- src/components/owner/Sidebar.jsx
+++ src/components/owner/Sidebar.jsx
@@ -34,9 +34,9 @@ const Sidebar = () => {
             {
                 ownerMenuLinks.map((link, index) => (
                     <NavLink key={index} to={link.path} className={`relative flex items-center gap-2 w-full py-3 pl-4 first:mt-6 ${link.path === location.pathname ? "bg-primary/10 text-primary" : "text-gray-600"}`}>
-                        <img src={link.path === location.pathname ? link.coloredIcon : link.icon} alt="car icon" />
+                        <img src={link.path === location.pathname ? link.coloredIcon : link.icon} alt="sidebar icon" />
                         <span className="max-md:hidden">{link.name}</span>
-                        <div className={`${link.path === location.pathname && 'bg-primary'} w-1.5 h-8 rounded-l right-0 absolute`}></div>
+                        <div className={`${link.path === location.pathname && 'bg-primary'} w-1.5 h-11 rounded-l right-0 absolute`}></div>
                     </NavLink>
                 ))
             }
