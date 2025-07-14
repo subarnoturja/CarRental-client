7bf71ae7d0b4a14eb75aa1e4cd9d2e461e09883d
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-14 22:19:16 +0600

    user dashboard photo connect to the backend

--- src/components/owner/Sidebar.jsx
+++ src/components/owner/Sidebar.jsx
@@ -1,16 +1,33 @@
 import { NavLink, useLocation } from "react-router-dom";
-import { assets, dummyUserData, ownerMenuLinks } from "../../assets/assets"
+import { assets, ownerMenuLinks } from "../../assets/assets"
 import { useState } from "react";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
 
-    const user = dummyUserData;
+    const { user, axios, fetchUser } = useAppContext()
     const location = useLocation();
     const [image, setImage] = useState('');
 
     const updateImage = async () => {
-        user.image = URL.createObjectURL(image)
-        setImage('')
+        try {
+            const formData = new FormData()
+            formData.append('image', image)
+
+            const { data } = await axios.post('/api/owner/update-image', formData)
+
+            if(data.success){
+                fetchUser()
+                toast.success(data.message)
+                setImage('')
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
 
   return (
@@ -26,7 +43,7 @@ const Sidebar = () => {
         </div>
         {
             image && (
-                <button className="absolute top-0 right-0 flex p-2 gap-1 bg-primary/10 text-primary cursor-pointer">Save <img src={assets.check_icon} width={13} alt="check icon" onChange={updateImage} /></button>
+                <button className="absolute top-0 right-0 flex p-2 gap-1 bg-primary/10 text-primary cursor-pointer" onClick={updateImage}>Save <img src={assets.check_icon} width={13} alt="check icon" /></button>
             )
         }
         <p className="mt-2 text-base max-md:hidden">{user?.name}</p>
