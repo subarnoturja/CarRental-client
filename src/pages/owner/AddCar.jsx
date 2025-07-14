defd0c8dc458507ca10c12ccd3524d76f725f8a7
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-15 00:02:35 +0600

    add car of admin connected to backend

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import Title from "../../components/owner/Title";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const AddCar = () => {
 
-  const currency = import.meta.env.VITE_CURRENCY;
+  const { axios, currency } = useAppContext()
 
   const [image, setImage] = useState(null);
   const [car, setCar] = useState({
@@ -20,8 +22,47 @@ const AddCar = () => {
     description: '',
   })
 
-  const onSubmitHandler = () => {
+  const [isLoading, setIsLoading] = useState(false)
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    if(isLoading) return null
+    
+    setIsLoading(true)
+    try {
+      const formData = new FormData()
+      formData.append('image', image)
+      formData.append('carData', JSON.stringify(car))
+
+      const { data } = await axios.post('/api/owner/add-car', formData)
+
+      if(data.success){
+        toast.success(data.message)
+        setImage(null)
+        setCar({
+          brand: '',
+          model: '',
+          year: 0,
+          pricePerDay: 0,
+          category: '',
+          transmission: '',
+          fuel_type: '',
+          seating_capacity: 0,
+          location: '',
+          description: '',
+        })
+      }
+      else{
+        toast.error(data.message)
+        console.log(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error.message)
+    }
+    finally{
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -120,7 +161,7 @@ const AddCar = () => {
           </div>
           <button className="flex items-center gap-2 px-4 py-2.5 mt-4 bg-primary text-white rounded-md font-medium w-max cursor-pointer">
             <img src={assets.tick_icon} alt="tick icon" />
-            List Your Car
+            {isLoading ? 'Listing...' : 'List Your Car'}
           </button>
       </form>
     </div>
