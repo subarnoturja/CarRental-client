0fce243754f09f9ae0c73f57f2c8d77ce2f5c8a2
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-15 21:10:25 +0600

    animation added in my bookings page

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -3,6 +3,7 @@ import { assets } from "../assets/assets"
 import Title from "../components/Title"
 import { useAppContext } from "../context/AppContext"
 import toast from "react-hot-toast"
+import { motion } from 'motion/react';
 
 const MyBookings = () => {
 
@@ -29,11 +30,20 @@ const MyBookings = () => {
   }, [user])
 
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
+    <motion.div 
+    initial={{ opacity: 0, y: 30 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6 }}
+
+    className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
       <Title title="My Bookings" subTitle="View and manage your all car bookings" align="left" />
       <div>
         {bookings.map((booking, index) => (
-          <div key={index} className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12">
+        <motion.div 
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: index * 0.1, duration: 0.4 }}
+          key={index} className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12">
             {/* Car Image & Info */}
             <div className="md:col-span-1">
               <div className="rounded-md overflow-hidden mb-3">
@@ -73,10 +83,10 @@ const MyBookings = () => {
                 <p>Booked on {booking.createdAt.split('T')[0]}</p>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
