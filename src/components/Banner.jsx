16ee58329426b630d5dae18381a2c12713a9cdc8
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-15 20:18:53 +0600

    animation added in featured section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import { assets } from '../assets/assets'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558FE] to-[#A9CFFF] max-w-6xl mx-3 md:ax-auto rounded-2xl overflow-hidden'>
+    <div className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558FE] to-[#A9CFFF] max-w-7xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
         <div className='text-white'>
             <h2 className='text-3xl font-medium'>Do you Own a Luxury Car?</h2>
             <p className='mt-2'>Monetize your vehicle effortlessly by listing it on CarRental.</p>
