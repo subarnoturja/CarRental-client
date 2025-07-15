076178298e2210922695696b51d91961f70ff98e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-15 20:50:23 +0600

    animation added in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,85 @@
 import { assets } from "../assets/assets"
+import { motion } from 'motion/react';
 
 const Footer = () => {
   return (
-    <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-60 text-sm text-gray-500'>
-            <div className='flex flex-wrap justify-between items-start gap-8 pb-6 border-borderColor border-b'>
+    <motion.div 
+    initial={{ opacity: 0, y: 20 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6 }}
+    className='px-6 md:px-16 lg:px-24 xl:px-32 mt-60 text-sm text-gray-500'>
+            <motion.div 
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.3 }}
+            className='flex flex-wrap justify-between items-start gap-8 pb-6 border-borderColor border-b'>
                 <div>
-                    <img src={assets.logo} alt="logo" className='h-8 md:h-9' />
-                    <p className='max-w-80 mt-3'>
+                    <motion.img 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1}}
+                    transition={{ duration: 0.6, delay: 0.4 }}
+                    src={assets.logo} alt="logo" className='h-8 md:h-9' />
+
+                    <motion.p 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.6, delay: 0.4 }}
+                    className='max-w-80 mt-3'>
                         Premium car rental service with a wide selection of luxury and everyday vehicles for all your driving needs.
-                    </p>
-                    <div className='flex items-center gap-3 mt-6'>
+                    </motion.p>
+
+                    <motion.div 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.6, delay: 0.5 }}
+                    className='flex items-center gap-3 mt-6'>
                         {/* Social Media Icon */}
                         <a href="#"><img src={assets.facebook_logo} alt="facebook icon" /></a>
                         <a href="#"><img src={assets.instagram_logo} alt="facebook icon" /></a>
                         <a href="#"><img src={assets.twitter_logo} alt="facebook icon" /></a>
                         <a href="#"><img src={assets.gmail_logo} alt="facebook icon" /></a>
-                    </div>
+                    </motion.div>
                 </div>
 
-                <div>
-                    <h2 className='text-base font-medium text-gray-800 uppercase'>Quick Links</h2>
-                    <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Browse Cars</a></li>
-                        <li><a href="#">List Your Car</a></li>
-                        <li><a href="#">About Us</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 className='text-base font-medium text-gray-800 uppercase'>Resources</h2>
-                    <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li><a href="#">Help Center</a></li>
-                        <li><a href="#">Terms of Services</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Insurance</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
-                    <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li>1234 Luxury Drive</li>
-                        <li>San Francisco, CA 94107</li>
-                        <li>+1 234 567890</li>
-                        <li>info@example.com</li>
-                    </ul>
-                </div>
-            </div>
-            <div className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
+                <motion.div 
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: 0.4 }}
+                className="flex flex-wrap justify-between w-1/2 gap-8">
+                    <div>
+                        <h2 className='text-base font-medium text-gray-800 uppercase'>Quick Links</h2>
+                        <ul className='mt-3 flex flex-col gap-1.5'>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Browse Cars</a></li>
+                            <li><a href="#">List Your Car</a></li>
+                            <li><a href="#">About Us</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className='text-base font-medium text-gray-800 uppercase'>Resources</h2>
+                        <ul className='mt-3 flex flex-col gap-1.5'>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Terms of Services</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Insurance</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
+                        <ul className='mt-3 flex flex-col gap-1.5'>
+                            <li>1234 Luxury Drive</li>
+                            <li>San Francisco, CA 94107</li>
+                            <li>+1 234 567890</li>
+                            <li>info@example.com</li>
+                        </ul>
+                    </div>
+                </motion.div>
+            </motion.div>
+            <motion.div 
+            initial={{ opacity: 0, y: 10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.5 }}
+            className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
                 <p>Car Rental © {new Date().getFullYear()}. All rights reserved.</p>
                 <ul className='flex items-center gap-4'>
                     <li><a href="#">Privacy</a></li>
@@ -55,8 +88,8 @@ const Footer = () => {
                     <li>|</li>
                     <li><a href="#">Sitemap</a></li>
                 </ul>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
   )
 }
 
