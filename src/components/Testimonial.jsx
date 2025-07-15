e15df2bd91b86251b95be693be11063deac75f06
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-15 20:30:15 +0600

    animation added to the testimonial section

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,6 @@
 import { assets } from "../assets/assets";
 import Title from "./Title"
+import { motion } from 'motion/react';
 
 const Testimonial = () => {
 
@@ -27,7 +28,12 @@ const Testimonial = () => {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
                 {testimonials.map((testimonial, index) => (
-                    <div key={index} className="bg-white p-6 rounded-xl shadow-lg hover:-translate-y-1 transition-all duration-500">
+                    <motion.div 
+                    initial={{ opacity: 0, y: 40 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, delay: index * 0.2, ease: 'easeOut' }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    key={index} className="bg-white p-6 rounded-xl shadow-lg hover:-translate-y-1 transition-all duration-500">
                         <div className="flex items-center gap-3">
                             <img className="w-12 h-12 rounded-full" src={testimonial.image} alt={testimonial.name} />
                             <div>
@@ -41,7 +47,7 @@ const Testimonial = () => {
                             ))}
                         </div>
                         <p className="text-gray-500 max-w-90 mt-4 font-light">"{testimonial.testimonial}"</p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
